1c7949fef888c45e49da92be3557582633e81439
joao-aparecido <joaoaparecidoferreiraneto@gmail.com>
2025-08-20 12:23:32 -0300

    Primeira aula java

---
+++ ExecutarCalculadora.java
@@ -0,0 +1,18 @@
+import calculadora.Calculadora;
+
+public class ExecutarCalculadora {
+    
+    public static void main(String[] args){
+        Calculadora calc1 = new Calculadora();
+        Calculadora calc2 = new Calculadora();
+        calc1.numeroA = 2.0;
+        calc1.numeroB = 3.0;
+
+        calc2.numeroA = 4.0;
+        calc2.numeroB = 5.0;
+
+        calc1.somar();
+        calc2.somar();
+        
+    }
+}
