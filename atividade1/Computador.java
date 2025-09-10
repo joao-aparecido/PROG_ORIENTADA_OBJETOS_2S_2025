71ee672c15dec5ccf190448e28e5d9579146edbe
joao-aparecido <joaoaparecidoferreiraneto@gmail.com>
2025-09-10 11:07:50 -0300

    Atividade_01

---
+++ atividade1/Computador.java
@@ -0,0 +1,11 @@
+package atividade1;
+
+public class Computador {
+    public Integer memoriaRAM;
+    public Integer SSD;
+    public Integer nucleos;
+    public Float operacoesPorSegundo;
+
+    
+    
+}
---
+++ atividade1/Programa.java
@@ -0,0 +1,9 @@
+package atividade1;
+
+public class Programa {
+     public Integer memoriaRAMAlocada;
+    public Integer SSDOcupado;
+    public Integer nucleos;
+    public Float quantidadaOperacoes;
+    
+}
